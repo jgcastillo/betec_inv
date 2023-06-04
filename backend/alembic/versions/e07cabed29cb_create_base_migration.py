22bc8fcf7325a508d26a82096d81ba5c70da2d25
jgcastillop <jgcastillop@gmail.com>
2023-06-04 18:49:04 -0400

    initial commit

---
+++ backend/alembic/versions/e07cabed29cb_create_base_migration.py
@@ -0,0 +1,24 @@
+"""create base migration
+
+Revision ID: e07cabed29cb
+Revises: 
+Create Date: 2023-06-04 16:58:50.327495
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e07cabed29cb'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
